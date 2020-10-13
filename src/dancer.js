ac6ab1c488f490f582897da79e2de00f62bbb796
Simei Yeh <simei.yeh@gmail.com>
2020-10-12 20:36:26 -0700

    Updated dancefloor, blinkDancer.js, dancer.js

--- src/dancer.js
+++ src/dancer.js
@@ -7,7 +7,7 @@ var makeDancer = function (top, left, timeBetweenSteps) {
   this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
 
-  this.step();
+  //this.step();
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
